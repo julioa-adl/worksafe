394e8c0e78ba09cf14b861976bba94ace386ecac
Júlio Adler <julioader@gmail.com>
2023-02-15 10:34:46 -0300

    att julio link linkedin

--- src/components/Direitos.jsx
+++ src/components/Direitos.jsx
@@ -5,7 +5,7 @@ import '../styles/style.components/direitos.css';
 function Direitos() {
   return (
     <div className="direitos_container">
-      <p>© 2022 WORKSAFE BRASIL - Todos os direitos reservados - Desenvolvido por Júlio Adler</p>
+      <p>© 2022 WORKSAFE BRASIL - Todos os direitos reservados - Desenvolvido por <a className='julio' href='https://www.linkedin.com/in/julio-a-f-mesquita/' target='blank'>Júlio Adler</a></p>
     </div>
   )
 }
